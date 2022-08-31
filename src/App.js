fd849e70ef2d533091324a5a65ff2143eba4cadc
Mohsin ali <87361294+iMohsinali@users.noreply.github.com>
2022-08-31 13:19:33 +0500

    destructure in Components

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import {
   Routes,
   Route,
   NavLink,
-  Link,
-  Outlet,
-  useParams,
 } from "react-router-dom";
 
+import Home from "./components/Home";
+import Launch from "./components/Launch";
+import LaunchIndex from "./components/LaunchIndex";
+import LaunchShoe from "./components/LaunchShoe";
 export default function App() {
   return (
     <Router>
@@ -55,87 +56,3 @@ export default function App() {
     </Router>
   );
 }
-
-function Home() {
-  return (
-    <div>
-      <h1>Welcome Home!</h1>
-      <p>
-        A simple shoe app that is created using react router. Click the Launch
-        in the navbar to see the shoes.
-      </p>
-    </div>
-  );
-}
-
-function Launch() {
-  return (
-    <div>
-      <h1>Launch</h1>
-      <Outlet />
-    </div>
-  );
-}
-
-function LaunchIndex() {
-  return (
-    <ul>
-      {Object.entries(shoes).map(([slug, { name, img }]) => (
-        <li key={slug}>
-          <NavLink
-            style={({ isActive }) => {
-              return {
-                display: "block",
-                margin: "1rem 1rem",
-                color: isActive ? "red" : "steelblue",
-                textDecoration: "none",
-              };
-            }}
-            to={`/Launch/${slug}`}
-          >
-            <h2>{name}</h2>
-            <img src={img} alt={name} />
-          </NavLink>
-        </li>
-      ))}
-    </ul>
-  );
-}
-
-function LaunchShoe() {
-  const { slug } = useParams();
-  const shoe = shoes[slug];
-
-  if (!shoe) {
-    return <h2>Not Found</h2>;
-  }
-
-  const { name, img } = shoe;
-
-  return (
-    <div>
-      <p>{name}</p>
-      <img src={img} alt={name} />
-      <p>
-        <Link to="/Launch" element={<Launch />}>
-          Back to Launch
-        </Link>
-      </p>
-    </div>
-  );
-}
-
-const shoes = {
-  "air-jordan-3-valor-blue": {
-    name: "VALOUR BLUE",
-    img: "https://secure-images.nike.com/is/image/DotCom/CT8532_104_A_PREM?$SNKRS_COVER_WD$&align=0,1",
-  },
-  "jordan-mars-270-london": {
-    name: "JORDAN MARS 270 LONDON",
-    img: "https://secure-images.nike.com/is/image/DotCom/CV3042_001_A_PREM?$SNKRS_COVER_WD$&align=0,1",
-  },
-  "air-jordan-1-zoom-racer-blue": {
-    name: "RACER BLUE",
-    img: "https://secure-images.nike.com/is/image/DotCom/CK6637_104_A_PREM?$SNKRS_COVER_WD$&align=0,1",
-  },
-};
